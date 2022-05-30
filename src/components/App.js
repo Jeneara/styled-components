2d0dd3a0c74539a1376be6953a5ffeccf25055df
Steph Mason <stephanie.mason@99designs.com>
2022-05-30 16:51:23 +1000

    create global style

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
+import React from "react";
+import {Button} from 'components/common';
+import {createGlobalStyle} from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: #fff;
+    min-height: 100vh;
+    margin: 0;
+    color: #000;
+    font-family: 'Kaushan Script';
+  }
+
+`;
 
 function App() {
   return (
+    <>
+    <GlobalStyle />
     <h1>
       App
     </h1>
+    <Button>
+      Test
+    </Button>
+    </>
   );
 }
 
